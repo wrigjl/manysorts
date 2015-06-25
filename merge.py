e37a7f643dfb045a6e709360ce493413d46a213b
Jason L. Wright <jason@thought.net>
2015-06-25 08:32:51 -0600

    - add copyright - mark sorting thread as daemon (so we can exit early if needed) - use random.shuffle instead of numpy (one less dependency)

--- merge.py
+++ merge.py
@@ -1,5 +1,31 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2015, Jason L. Wright <jason@thought.net>
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import numpy
+# implementation of visualized merge sort
+
 import matplotlib
 matplotlib.use('TKAgg')
 import matplotlib.pyplot as plt
@@ -11,7 +37,6 @@ import copy
 import random
 
 resQueue = Queue.Queue(10)
-#theArray = numpy.random.permutation(1000)
 theArray = range(1000)
 random.shuffle(theArray)
 
@@ -52,6 +77,7 @@ def sort_thread():
 threads = []
 for target in (sort_thread,):
     t = threading.Thread(target=target)
+    t.daemon = True
     threads.append(t)
 
 def do_merge(q, a, Z, base):
@@ -88,5 +114,3 @@ def do_merge(q, a, Z, base):
 ani = animation.FuncAnimation(fig, update, fargs=(aargs,), interval=10,
                               init_func=init, blit=True)
 plt.show()
-
-[t.join() for t in threads]
--- qsort.py
+++ qsort.py
@@ -1,5 +1,31 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2015, Jason L. Wright <jason@thought.net>
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import numpy
+# implementation of visualized quick sort
+
 import matplotlib
 matplotlib.use('TKAgg')
 import matplotlib.pyplot as plt
@@ -8,9 +34,11 @@ import pprint
 import threading
 import Queue
 import copy
+import random
 
 resQueue = Queue.Queue(10)
-theArray = numpy.random.permutation(1000)
+theArray = range(1000)
+random.shuffle(theArray)
 
 fig = plt.figure()
 ax = plt.axes(xlim=(0, len(theArray)), ylim=(0, len(theArray)))
@@ -44,6 +72,7 @@ def sort_thread():
 threads = []
 for target in (sort_thread,):
     t = threading.Thread(target=target)
+    t.daemon = True
     threads.append(t)
 
 def do_qsort(q, a, lo, hi):
@@ -75,5 +104,3 @@ def partition(q, a, lo, hi):
 ani = animation.FuncAnimation(fig, update, blit=True, init_func=init,
                               fargs=(aargs,), interval=10)
 plt.show()
-
-[t.join() for t in threads]
